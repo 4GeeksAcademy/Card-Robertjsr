f9686988b21ad2dc5bf70489e1658e763a7b0ced
Robertjsr <Eryero02@gmail.com>
2025-03-21 11:44:19 +0000

    Javascript Finalizado

--- src/app.js
+++ src/app.js
@@ -6,5 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  document.querySelector('.card').classList.add('heart');
+  document.querySelector('.card').classList.add(generateRandomSimbols());
+  document.querySelector('.card').innerHTML = generateRandomNumber();
 };
+
+let generateRandomNumber = () =>{
+  let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let numerosRandom = Math.floor(Math.random()* numeros.length);
+  return numeros[numerosRandom];
+}
+
+let generateRandomSimbols = () =>{
+  let simbolos = ["diamond", "spade", "heart", "club"];
+  let simbolosRandom = Math.floor(Math.random()* simbolos.length);
+  return simbolos[simbolosRandom];
+}
